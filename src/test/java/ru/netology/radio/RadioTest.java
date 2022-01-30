0ccdc8a95efb57997b3bce254539b6073866b775
vvitoss <orangekaramel@gmail.com>
2022-01-30 19:24:37 +0300

    update Radio @ RadioTest

---
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -0,0 +1,40 @@
+package ru.netology.radio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void getCurrentChanel() {
+    }
+
+    @Test
+    void setCurrentChanel() {
+    }
+
+    @Test
+    void next() {
+    }
+
+    @Test
+    void prev() {
+    }
+
+    @Test
+    void getCurrentVolume() {
+    }
+
+    @Test
+    void setCurrentVolume() {
+    }
+
+    @Test
+    void up() {
+    }
+
+    @Test
+    void down() {
+    }
+}
