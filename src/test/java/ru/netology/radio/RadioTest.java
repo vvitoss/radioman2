c8a28fbc42c39087da56e854a2a90ef84d524c06
vvitoss <orangekaramel@gmail.com>
2022-01-30 21:16:16 +0300

    update RadioTest

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -1,40 +1,82 @@
 package ru.netology.radio;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
 
-    @Test
-    void getCurrentChanel() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_getCurrentChanel.csv", delimiter = '|')
+    void getCurrentChanel(String testName, int currentChanel, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentChanel(currentChanel);
+
+        assertEquals(expected, radio.getCurrentChanel());
     }
 
-    @Test
-    void setCurrentChanel() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_setCurrentChanel.csv", delimiter = '|')
+    void setCurrentChanel(String testName, int currentChanel, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentChanel(currentChanel);
+
+        assertEquals(expected, radio.getCurrentChanel());
     }
 
-    @Test
-    void next() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_next.csv", delimiter = '|')
+    void next(String testName, int currentChanel, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentChanel(currentChanel);
+        radio.next();
+
+        assertEquals(expected, radio.getCurrentChanel());
     }
 
-    @Test
-    void prev() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_prev.csv", delimiter = '|')
+    void prev(String testName, int currentChanel, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentChanel(currentChanel);
+        radio.prev();
+
+        assertEquals(expected, radio.getCurrentChanel());
     }
 
-    @Test
-    void getCurrentVolume() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_getCurrentVolume.csv", delimiter = '|')
+    void getCurrentVolume(String testName, int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(currentVolume);
+        assertEquals(expected, radio.getCurrentVolume());
     }
 
-    @Test
-    void setCurrentVolume() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_setCurrentVolume.csv", delimiter = '|')
+    void setCurrentVolume(String testName, int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(currentVolume);
+        assertEquals(expected, radio.getCurrentVolume());
     }
 
-    @Test
-    void up() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_up.csv", delimiter = '|')
+    void up(String testName, int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(currentVolume);
+        radio.up();
+        assertEquals(expected, radio.getCurrentVolume());
     }
 
-    @Test
-    void down() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data_down.csv", delimiter = '|')
+    void down(String testName, int currentVolume, int expected) {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(currentVolume);
+        radio.down();
+        assertEquals(expected, radio.getCurrentVolume());
     }
 }
